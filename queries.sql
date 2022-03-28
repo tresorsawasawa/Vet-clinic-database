f265602afd024b607dc1c7f3e923eac772a4425f
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 22:32:04 +0200

    Add command to select animals names that are neutered and have less than 3 escape attempts

--- queries.sql
+++ queries.sql
@@ -2,3 +2,4 @@
 
 SELECT * FROM animals WHERE name LIE '%mon';
 SELECT name FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
+SELECT name  FROM animals WHERE neutered = true  AND escape_attemps < 3;
