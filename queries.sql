c48a418c327c0ac58dc455d97cb31652299dd418
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 23:43:15 +0200

    Correct a typo error: 'LIKE' instead of 'LIE'

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIE '%mon';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
 SELECT name  FROM animals WHERE neutered = true  AND escape_attemps < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
