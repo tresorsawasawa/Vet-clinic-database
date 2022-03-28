f71bfc7f43b7fcc675ef7abbe971a4e0122d2833
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 22:39:59 +0200

    Add command to select animals names that have more then 10.5kg

--- queries.sql
+++ queries.sql
@@ -4,3 +4,4 @@ SELECT * FROM animals WHERE name LIE '%mon';
 SELECT name FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
 SELECT name  FROM animals WHERE neutered = true  AND escape_attemps < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
+SELECT name FROM animals WHERE weight_kg > 10.5;
