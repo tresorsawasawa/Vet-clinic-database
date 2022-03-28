421f47ee12eea3d97f0babeeac140c3ed1fd4a1f
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 22:24:51 +0200

    Add command to select animals borned between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIE '%mon';
+SELECT *  FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
