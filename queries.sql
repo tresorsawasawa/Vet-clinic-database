b401c3a5c88d14f60d977a1ecc2308d34229cc9a
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 23:47:41 +0200

    Correct the date stucture

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
+SELECT name FROM animals WHERE date_of_birth  BETWEEN '2016/01/01' AND '2019/12/31';
 SELECT name  FROM animals WHERE neutered = true  AND escape_attemps < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
 SELECT name FROM animals WHERE weight_kg > 10.5;
