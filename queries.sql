0f5c07033fac31068a3e7a2dfbf85e8e3497cc9c
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-28 22:41:38 +0200

    Add command to Find all animals that are neutered

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT name FROM animals WHERE date_of_birth  BETWEEN '20160101' AND '20190101';
 SELECT name  FROM animals WHERE neutered = true  AND escape_attemps < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
 SELECT name FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
