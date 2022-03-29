987eed240ec1ebd37df198bcb2fac2992e1c3a45
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-29 14:53:57 +0200

    delete all records in the animals table, then roll back the transaction.

--- queries.sql
+++ queries.sql
@@ -10,8 +10,12 @@ SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 /* Start the Transaction */
-
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
+
+/* Start the Transaction */
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
