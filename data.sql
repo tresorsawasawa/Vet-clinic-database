26f489a41a3aed2641c352295b9dc1028cd13cce
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-31 10:45:29 +0200

    Modify the inserted animals to include owner information 'owner_id'

--- data.sql
+++ data.sql
@@ -18,7 +18,7 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23 ),
 
 /*--- Insert Data into the owners table ----*/
 INSERT INTO owners(full_name, age)
-VALUES('Sam', 34),
+VALUES('Sam Smith', 34),
       ('Jennifer Orwell', 19),
       ('Bob', 45),
       ('Melody Pond', 77),
@@ -50,9 +50,10 @@ WHERE
 COMMIT;
 
 /*---- Modify the inserted animals to include owner information 'owner_id' -----*/
-
-UPDATE animals SET owner_id = 'Sam Smith ' WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 'Jennifer Orwell ' WHERE name = 'Pikachu' OR name ='Gabumon';
-UPDATE animals SET owner_id = 'Bob' WHERE name = 'Devimon' OR name = 'Plantmon';
-UPDATE animals SET owner_id = 'Melody Pond ' WHERE name = 'Charmander' OR name ='Squirtle' OR name ='Blossom';
-UPDATE animals SET owner_id = 'Dean Winchester' WHERE name = 'Angemon' OR name = 'Boarmon';
+BEGIN;
+UPDATE animals SET owner_id = 1 WHERE name IN ('Agumon');
+UPDATE animals SET owner_id = 2 WHERE name IN ('Pikachu', 'Gabumon');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+COMMIT;
