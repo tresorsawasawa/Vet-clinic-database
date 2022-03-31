3d44e69ee74d11ee9a2bcccfcb8fbdd4fba3c37e
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-31 23:50:23 +0200

    Insert data in the 'specializations' table

--- data.sql
+++ data.sql
@@ -58,9 +58,15 @@ UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blosso
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 COMMIT;
 
-/* Insert data in the vets table*/
+/* Insert data in the 'vets' table*/
 INSERT INTO vets(name, age, date_of_graduation)
 VALUES('William Tatcher', 45, '2000-04-23'),
       ('Maisy Smith', 26, '2019-01-17'),
       ('Stephanie Mendez', 64, '1981-05-04'),
       ('Jack Harkness', 38, '2008-06-08');
+
+/* Insert data in the 'specializations' table*/
+INSERT INTO specializations(vets_id, species_id)
+VALUES(1,1),
+      (3,2),
+      (4,2);
